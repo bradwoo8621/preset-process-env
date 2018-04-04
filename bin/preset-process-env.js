aa368ca8d93fc5cd3709deffbccd519391d1d929
bradwoo8621 <bradwoo8621@gmail.com>
2018-04-04 14:40:42 +0800

    codacy

--- bin/preset-process-env.js
+++ bin/preset-process-env.js
@@ -11,13 +11,13 @@ console.log(`Custom script[${script}] run for environment[${env}].`);
 
 const appDirectory = fs.realpathSync(process.cwd());
 console.log(`Application directory is [${appDirectory}].`);
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 const envFile = resolveApp(`envs/.env.${env}`);
 console.log(`Environment file is [${envFile}].`);
 const environmentVariables = require(envFile);
 // 复制环境参数到process.env, 可能会覆盖全局参数
-Object.keys(environmentVariables).forEach(key => {
+Object.keys(environmentVariables).forEach((key) => {
 	process.env[key] = environmentVariables[key];
 });
 
@@ -27,9 +27,9 @@ Object.keys(environmentVariables).forEach(key => {
 // 如果y未字符串false, 则设置为boolean值false
 // 其他y值不变, 以字符串形式设置
 if (variables) {
-	variables.forEach(variable => {
+	variables.forEach((variable) => {
 		let parsed = variable.split('=');
-		if (parsed.length == 1 || parsed[1] === 'true') {
+		if (parsed.length === 1 || parsed[1] === 'true') {
 			process.env[parsed[0]] = true;
 		} else if (parsed[1] === 'false') {
 			process.env[parsed[0]] = false;
@@ -41,13 +41,13 @@ if (variables) {
 
 const result = spawn.sync('npm', ['run', script], { stdio: 'inherit' });
 if (result.signal) {
-	if (result.signal == 'SIGKILL') {
+	if (result.signal === 'SIGKILL') {
 		console.log(
 			'The build failed because the process exited too early. ' +
 				'This probably means the system ran out of memory or someone called ' +
 				'`kill -9` on the process.'
 		);
-	} else if (result.signal == 'SIGTERM') {
+	} else if (result.signal === 'SIGTERM') {
 		console.log(
 			'The build failed because the process exited too early. ' +
 				'Someone might have called `kill` or `killall`, or the system could ' +
